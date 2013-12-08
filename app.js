7286900b4c4c95ab8db3ecd42bfcaa3942b3eb62
Lisa Sileo <lxs4d554@email.cpcc.edu>
2013-12-07 22:09:51 -0500

    update and delete functionality added

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var path = require('path');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/crud_example');
-
+var id;
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 5000);
@@ -37,7 +38,11 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/samplegroup', routes.samplegroup(db));
 app.get('/newsample', routes.newsample);
+app.get('/editsample/:id', routes.editsample(db));
+
+app.post('/update', routes.update(db));
 app.post('/addsample', routes.addsample(db));
+app.post('/deletesample', routes.deletesample(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ exports.addsample = function(db) {
             "title" : title,
             "medium" : medium,
 			"artist" : artist,
-			"displayed" : false
+			"displayed" : "no"
         }, function (err, doc) {
             if (err) {
                 // If it failed, return error
@@ -50,7 +50,88 @@ exports.addsample = function(db) {
 
     }
 }
-
+ 
 exports.newsample = function(req, res){
   res.render('newsample', { title: 'CRUD Example - Create' });
 };
+
+exports.editsample = function(db){
+	return function(req, res) {
+	
+		var collection= db.get('artcollection');
+		collection.find({"_id": req.params.id}, {}, 
+			function(e, docs) {
+				res.render('editsample', { title: 'Find' + docs.length, "editsample" : docs });
+			});
+		
+	};
+};
+
+exports.update = function(db){
+	return function(req, res) {
+
+        // Get our form values. These rely on the "name" attributes
+        var title = req.body.title;
+        var medium = req.body.medium;
+		var artist = req.body.artist;
+		var displayed = req.body.displayed;
+		var id = req.body.id;
+		
+        // Set our collection
+        var collection = db.get('artcollection');
+
+        // Submit to the DB
+        collection.update({"_id": id},{
+            "title" : title,
+            "medium" : medium,
+			"artist" : artist,
+			"displayed" : displayed
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+				//res.location("samplegroup");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("samplegroup");
+                // And forward to success page
+                res.redirect("samplegroup");
+            }
+        });
+
+    }
+};
+
+exports.deletesample = function(db){
+	return function(req, res) {
+
+        // Get our form values. These rely on the "name" attributes
+        
+		var id = req.body.id2;
+		
+        // Set our collection
+        var collection = db.get('artcollection');
+
+        // Submit to the DB
+       collection.remove({"_id": id}
+	   , function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+				//res.location("samplegroup");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("samplegroup");
+                // And forward to success page
+                res.redirect("samplegroup");
+            }
+        });
+
+    }
+};
+
+
+
+
